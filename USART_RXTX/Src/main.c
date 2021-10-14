220fa7aa4321a8065bede2972f01c48f71f82670
KnowingBat <d.dangelo@studenti.unibs.it>
2021-10-14 15:32:45 +0200

    Aggiunta ADC e USART_RXTX

---
+++ USART_RXTX/Src/main.c
@@ -0,0 +1,30 @@
+#include <stdint.h>
+#include "stm32f411xe.h"
+#include <stdio.h>
+#include "uart.h"
+
+#define GPIOAEN 	(1U<<0)
+#define GPIOA_5		(1U<<5)
+#define LED_PIN		GPIOA_5
+
+char key;
+
+int main(void){
+
+	RCC->AHB1ENR |= GPIOAEN;
+
+	GPIOA->MODER |= (1U<<10);
+	GPIOA->MODER &= ~(1U<<11);
+
+	uart2_init();
+
+	while(1){
+		key = uart2_read();
+		if(key == '1'){
+			GPIOA->ODR = LED_PIN;
+		}else {
+			GPIOA->ODR &= ~LED_PIN;
+				}
+	}
+}
+
